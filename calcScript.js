fa55c57e4ba6e9e19781c1dffa0587c9652952a3
MthBarber <mthbarber@gmail.com>
2022-01-04 17:59:47 +0000

    Begin adding code for functionality for form

--- calcScript.js
+++ calcScript.js
@@ -1,7 +1,20 @@
+//Make array to store values to make a leaderboard
+const leaderboard = [];
+//Get access to values in form
+const distance = document.getElementById('distance');
+const fuelType = document.getElementById('fuelType');
+const passengers = document.getElementById('passengers');
+
 const calculateButton = document.getElementById('submit');
 
 calculateButton.addEventListener("click", (e) => {
-    
-    alert("I just calculated");
+    e.preventDefault();
+    calculateFunction();
     
 })
+
+const calculateFunction = (distance,fuelType,passengers) =>{
+    let emissions = (distance * fuelType) / passengers;
+        console.log(typeof(distance));
+    return console.log(emissions);
+}
