9d5ed44621d4585775b7c9e5fbdc782ac98c34dd
MthBarber <mthbarber@gmail.com>
2022-01-05 17:10:19 +0000

    Early build of calculator and out now being produced to the DOM

--- calcScript.js
+++ calcScript.js
@@ -1,23 +1,28 @@
 //Make array to store values to make a leaderboard
 const leaderboard = [];
 
+
 //Get calculate button from HTML
 const calculateButton = document.getElementById('submit');
 
 calculateButton.addEventListener("click", (e) => {
     //Get access to values in form
 const distance = document.getElementById('distance').value;
-const fuelType = document.getElementById('fuelType').value;
+const vehicle = document.getElementById('vehicle').value;
 const passengers = document.getElementById('passengers').value;
     e.preventDefault();
-    calculateFunction(distance,fuelType,passengers);
+    calculateFunction(distance,vehicle,passengers);
     
 })
 
-const calculateFunction = (distance,fuelType,passengers) =>{
-    const formOutput = document.getElementsByClassName("formOutput");
-    let emissions = distance  / passengers;
+const calculateFunction = (distance,vehicle,passengers) =>{     
+    let formOutput = document.getElementById("formOutput");
+    let emissions = ((distance * vehicle) / passengers) / 1000 ;
     leaderboard.push(emissions);
     leaderboard.sort();
-    return console.table(leaderboard) + console.log(emissions);
+    let div = document.createElement('div');
+    let textNode = document.createTextNode("You produced " + emissions + "kg of CO2 for this journey");
+    div.appendChild(textNode);    
+    formOutput.appendChild(div);
+    return console.table(leaderboard) + console.log(vehicle) ;
 }
