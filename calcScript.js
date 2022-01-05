6d80868a0daca3a17fa3e3d466e73dd84c058c6e
MthBarber <mthbarber@gmail.com>
2022-01-05 11:32:38 +0000

    Form beginning to function

--- calcScript.js
+++ calcScript.js
@@ -1,20 +1,24 @@
 //Make array to store values to make a leaderboard
 const leaderboard = [];
-//Get access to values in form
-const distance = document.getElementById('distance');
-const fuelType = document.getElementById('fuelType');
-const passengers = document.getElementById('passengers');
 
+//Get calculate button from HTML
 const calculateButton = document.getElementById('submit');
 
 calculateButton.addEventListener("click", (e) => {
+    //Get access to values in form
+const distance = document.getElementById('distance').value;
+const fuelType = document.getElementById('fuelType').value;
+const passengers = document.getElementById('passengers').value;
     e.preventDefault();
-    calculateFunction();
+    calculateFunction(distance,fuelType,passengers);
     
 })
 
 const calculateFunction = (distance,fuelType,passengers) =>{
-    let emissions = (distance * fuelType) / passengers;
+    let emissions = distance  / passengers;
         console.log(typeof(distance));
-    return console.log(emissions);
+        console.log("distance is..." + distance)
+        console.log(emissions);
+        let intEmissions = parseInt(emissions)
+    return console.log(emissions) + console.log(intEmissions);
 }
