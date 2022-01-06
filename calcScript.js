27a20bd7c74904deb9fe119c4f9c0d60546c55d7
MthBarber <mthbarber@gmail.com>
2022-01-06 11:17:59 +0000

    Begin adding code for check correct inputs and using checkbox to show passengers field

--- calcScript.js
+++ calcScript.js
@@ -18,9 +18,9 @@ const passengers = document.getElementById('passengers').value;
 const calculateFunction = (distance,vehicle,passengers) =>{     
     let formOutput = document.getElementById("formOutput");
     let emissions = ((distance * vehicle) / passengers) / 1000 ; //algorithm for CO2 created
-    leaderboard.push(emissions);
-    leaderboard.sort((a, b)=> a - b); //sort Array for leaderboard score
     let div = document.createElement('div');
+    leaderboard.push(emissions);
+    leaderboard.sort((a, b)=> a - b); //sort Array for leaderboard score    
     let textNode = document.createTextNode("You produced " + emissions + "kg of CO2 for this journey");
     let currentPosition = leaderboard.indexOf(emissions);
     let leaderNode = document.createTextNode("You are position: " + (currentPosition + 1) + " on our leaderboard");
@@ -29,3 +29,33 @@ const calculateFunction = (distance,vehicle,passengers) =>{
     formOutput.appendChild(leaderNode);
     return console.table(leaderboard);
 }
+
+
+const checkbox = document.getElementById('checkbox');
+const passengersLabel = document.getElementById('passengersLabel').value;
+const passengers = document.getElementById('passengers').value;
+
+checkbox.addEventListener('change', (e)=>{
+    if (this.checked){
+        checkbox.value = "unchecked"
+        console.log("not checked");
+        passengersLabel.style.display = "flex";
+    }else {
+        console.log("checked");
+        checkbox.value = "checked";
+    }
+});
+
+//Begin code for making switch statement for number of passengers.
+
+switch (passengers) {
+    case NaN:
+        alert("please enter a number.");
+        break;
+    case 0:
+        alert("Please enter a number greater than zero.")
+        break;
+    
+
+}
+
