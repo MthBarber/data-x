b98c20b21885ef14277701ac47d3ac54736b629d
patrick-watsonica <93407869+patrick-watsonica@users.noreply.github.com>
2022-01-18 17:46:39 +0000

    Added Simulator to simulator page

--- app.js
+++ app.js
@@ -16,3 +16,114 @@ navToggle.addEventListener('click', () => {
 		document.body.style.overflow = 'auto';
 	}
 });
+
+/////////////SIMULATOR CODE ///////////////////////////////
+
+const img01 = document.querySelector('.city');
+const btnReset = document.querySelector('#btn-reset');
+const btn01 = document.querySelector('#btn-01');
+const btn02 = document.querySelector('#btn-02');
+const btn03 = document.querySelector('#btn-03');
+const btn04 = document.querySelector('#btn-04');
+const trees = document.querySelector('.trees-wrap');
+const info = document.querySelector('.info-text');
+const years = document.querySelector('.years');
+const kgs = document.querySelector('.kgs');
+
+let control = 0;
+let milesCtrl = 0;
+let carCtrl = 0;
+
+const setControl = function () {
+	if (milesCtrl === 1 && carCtrl === 1) {
+		control = 1;
+		img01.src = 'images/icon-city-fog-30.png';
+		years.innerHTML = '1';
+		kgs.innerHTML = '200';
+	} else if (milesCtrl === 1 && carCtrl === 2) {
+		control = 2;
+		img01.src = 'images/icon-city-fog-50.png';
+		years.innerHTML = '2';
+		kgs.innerHTML = '400';
+	} else if (milesCtrl === 2 && carCtrl === 1) {
+		control = 3;
+		img01.src = 'images/icon-city-fog-65.png';
+		years.innerHTML = '10';
+		kgs.innerHTML = '2000';
+	} else if (milesCtrl === 2 && carCtrl === 2) {
+		control = 4;
+		img01.src = 'images/icon-city-fog-95.png';
+		years.innerHTML = '20';
+		kgs.innerHTML = '4000';
+	} else {
+		control = 0;
+		img01.src = 'images/icon-city-lights.png';
+	}
+};
+
+const displayTreesAndInfo = function () {
+	if (control > 0) {
+		trees.style.display = 'block';
+		info.style.display = 'block';
+	} else {
+		trees.style.display = 'none';
+		info.style.display = 'none';
+	}
+};
+
+let btnResetToggle = true;
+
+btnReset.addEventListener('click', () => {
+	milesCtrl = 0;
+	carCtrl = 0;
+	setControl();
+	displayTreesAndInfo();
+	if (btnResetToggle === true) {
+		btnReset.style.background = 'red';
+		btn01.style.background = 'white';
+		btn02.style.background = 'white';
+		btn03.style.background = 'white';
+		btn04.style.background = 'white';
+		btnResetToggle = false;
+		setTimeout(() => {
+			btnReset.style.background = 'hsl(var(--clr-light))';
+		}, 1000);
+		btnResetToggle = true;
+	} else {
+		btnReset.style.background = 'white';
+		btnResetToggle = true;
+	}
+	console.log(control);
+});
+
+btn01.addEventListener('click', () => {
+	milesCtrl = 1;
+	setControl();
+	displayTreesAndInfo();
+	btn01.style.background = 'yellow';
+	btn02.style.background = 'white';
+});
+
+btn02.addEventListener('click', () => {
+	milesCtrl = 2;
+	setControl();
+	displayTreesAndInfo();
+	btn01.style.background = 'white';
+	btn02.style.background = 'yellow';
+});
+
+btn03.addEventListener('click', () => {
+	carCtrl = 1;
+	setControl();
+	displayTreesAndInfo();
+	btn03.style.background = 'yellow';
+	btn04.style.background = 'white';
+});
+
+btn04.addEventListener('click', () => {
+	carCtrl = 2;
+	setControl();
+	displayTreesAndInfo();
+	btn03.style.background = 'white';
+	btn04.style.background = 'yellow';
+});
